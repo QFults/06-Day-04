921d5a63c391f58674b3a9cc770d391e39259240
Quinton Fults <quinton.fults.work@gmail.com>
2021-06-10 10:51:07 -0700

    code

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2')
+require('console.table')
 
 const db = mysql.createConnection('mysql://root:rootroot@localhost:3306/users_db')
 
-// db.query('SELECT * FROM users', (err, users) => {
-//   if (err) { console.log(err) }
-//   console.log(users)
-// })
+db.query('SELECT * FROM users', (err, users) => {
+  if (err) { console.log(err) }
+  console.table(users)
+})
 
 // const condition = { isSignedIn: false }
 
